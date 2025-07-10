2bb52ca134a523102521acd4f71b42f42e54cedb
tony96kimsh <tony96kimsh@gmail.com>
2025-07-10 12:23:45 +0900

    feat: Npgsql 연결 디버깅

--- Program.cs
+++ Program.cs
@@ -9,11 +9,19 @@ class Program
         // PostgreSQL 접속 문자열
         string connString = 
             "Host=localhost;" +
+            "Port=5342;" +  // 기본 포트 번호 5432 사용 시, 생략 가능
             "Username=postgres;" +
             "Password=post1234;" +
             "Database=postgres";
-        
-        using var conn = new NpgsqlConnection(connString);
-        conn.Open(); // 연결을 실제로 여는 함수        
+        try
+        {
+            using var conn = new NpgsqlConnection(connString);
+            conn.Open();
+            Console.WriteLine("PostgreSQL 연결 성공");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("PostgreSQL 연결 실패: " + ex.Message);
+        }        
     }
 }
